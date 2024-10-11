225e2a082c3f94ed603e30afb1ceb1a28b324a13
ouzepe <je.ouzepe@gmail.com>
2024-10-11 20:09:02 +0200

    menu

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { LinkSocials } from "data/LinkSocials";
+import { LinkSocials } from "~/data/LinkSocials";
 import Link from "next/link";
 import React from "react";
 
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import LogoOJL from "~/assets/ojl-white.svg";
 
 export const Nav = () => {
-  return <nav>Nav</nav>;
+  return (
+    <nav className='w-full fixed flex justify-center'>
+      <div className='w-[90vw] flex justify-between mt-4 text-xl font-bold uppercase'>
+        <Link href='#work'>Work</Link>
+        <div className='w-14 h-12 relative'>
+          <Image src={LogoOJL} alt='logo-ojl' fill quality={100} />
+        </div>
+        <Link href='#about'>About</Link>
+      </div>
+    </nav>
+  );
 };
