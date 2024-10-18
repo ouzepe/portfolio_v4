5b297088ff721fc34b6a5838efd911e450f3cb9f
ouzepe <je.ouzepe@gmail.com>
2024-10-19 00:22:23 +0200

    horizintal text update

--- components/ScrollHorizontalText.tsx
+++ components/ScrollHorizontalText.tsx
@@ -1,18 +1,21 @@
 "use client";
-import { useScroll } from "framer-motion";
+import { useScroll, useTransform, motion } from "framer-motion";
 import React from "react";
 
 const ScrollHorizontalText = () => {
   const { scrollXProgress } = useScroll();
-  //   const x = useTransform(scrollXProgress, [0, 1], [0, -600]);
-  console.log(scrollXProgress, "scrollXProgress");
+  const x1 = useTransform(scrollXProgress, [0, 1], [0, -120]);
+  console.log(x1, "x");
   const text =
     "React.js Next.js TailwindCSS Docker TypeScript Node.js Express.js MongoDB PostgreSQL GraphQL Prisma.js";
   return (
     <div className='w-screen mt-[15dvh]'>
-      <h1 className='text-[8dvw] lg:text-[4dvw] font-bold uppercase inline-block whitespace-nowrap overflow-hidden '>
+      <motion.h1
+        style={{ x: x1 }}
+        className='text-[8dvw] lg:text-[4dvw] font-thin uppercase inline-block whitespace-nowrap animate-defilement-rtl overflow-hidden '
+      >
         {text}
-      </h1>
+      </motion.h1>
     </div>
   );
 };
